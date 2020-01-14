ee14aa35279154fb1e556e379e60b7470babb555
Green <domenico.verde@aol.com>
2020-01-14 22:58:05 +0100

    - Setup

---
+++ ArkanoidClone/src/ui/GamePanel.java
@@ -0,0 +1,17 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+  GamePanel() {
+
+  }
+
+  @Override
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+
+
+  }
+}
